074742662ab1bb4589ee56f9dfc3cab1a09c3bca
diegobanos <diegobanosfarinas@gmail.com>
2016-12-23 15:07:41 -0500

    First commit

---
+++ spec/focus-spec.js
@@ -0,0 +1,25 @@
+'use babel';
+
+import Focus from '../lib/focus';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Focus', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('focus');
+  });
+
+  describe('when the focus:toggle event is triggered', () => {
+
+  });
+
+  describe('when the focus:destroy event is triggered', () => {
+
+  });
+});
